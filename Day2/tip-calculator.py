6d26fc9adef906f022394ca369fc3c8a508d4f7d
CarynMiel <121067393+CarynMiel@users.noreply.github.com>
2023-11-11 23:28:51 +0000

    Polishing up days 1-2

--- Day2/tip-calculator.py
+++ Day2/tip-calculator.py
@@ -7,4 +7,5 @@ bill = float(input("What was the total bill?: $"))
 tip = int(input("How much tip would you like to give?: ")) / 100
 people = int(input("How many people to split the bill?: "))
 pay = (bill * (1+tip)) / people
-print(f"Each person should pay: ${round(pay, 2)}")
+pay = "{:.2f}".format(pay)
+print(f"Each person should pay: ${pay}")
