06c396c9295354c935517528cc28744c3e32eb20
CarynMiel <121067393+CarynMiel@users.noreply.github.com>
2023-11-21 19:51:06 +0000

    adding a bit more

--- Day011/black-jack.py
+++ Day011/black-jack.py
@@ -2,6 +2,10 @@
 # Project: Black Jack
 import random
 
+import os
+clear = lambda: os.system('clear')
+clear()
+
 def main():
     play = input("Do you want to play Black Jack?: ")[0].lower()
 
@@ -12,29 +16,53 @@ def main():
         print("Ok bye!")
         quit()
     else:
-        play()
+        game()
 
-def play():
+def game():
+    clear()
     cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     player = [cards[random.randrange(0, len(cards))], cards[random.randrange(0, len(cards))]]
     pscore = player[0] + player[1]
     
     computer = [cards[random.randrange(0, len(cards))], cards[random.randrange(0, len(cards))]]
     cscore = computer[0] + computer[1]
+    while cscore < 11:
+        computer.append(cards[random.randrange(0, len(cards))])
+        cscore += computer[len(computer)-1]
     
-    print(f"Your cards are: [{player[0]}, {player[1]}]\nYour score is: {pscore}")
+    print(f"Your cards are: {player}\nYour score is: {pscore}")
     print(f"The computer's first card was: {computer[0]}\n")
+    play = "h"
     
-    play = input("Hit (get another card) or stand (keep current cards)?: ")[0].lower()
-    
-    while play != "h" and play != "s":
-        play = input("That is not an option. HIT or STAND")
+    while play != "s":
+        play = input("Hit (get another card) or stand (keep current cards)?: ")[0].lower()
         
-    if play == "s":
-        print(f"Your final score was: {pscore}")
-        print(f"Computer's score was: {cscore}")
-        
+        while play != "h" and play != "s":
+            play = input("That is not an option. HIT or STAND")
+            
+        if play == "s":
+            print(f"Your final score was: {pscore}")
+            print(f"Computer's score was: {cscore}")
+            checker(pscore, cscore)
+            
+        else:
+            player.append(cards[random.randrange(0, len(cards))])
+            pscore += player[len(player)-1]
+            if pscore > 21:
+                checker(pscore, cscore)
+            print(f"Your cards are: {player}\nYour score is: {pscore}")
+            print(f"The computer's first card is: {computer[0]}")
+            continue
+
+def checker(player, computer):
+    if player > 21:
+        print("You lose! It was a bust")
+    elif computer > 21:
+        print("You Win! The computer get a bust")
+    elif player > computer:
+        print("You Win! You got a closer score to 21")
     else:
-        player.append(cards[random.randrange(0, len(cards))])
+        print("You Lose! The computer got a closer score to 21")
+    quit()
         
 main()
